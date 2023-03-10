6bdc148e348a8c6b5bee53d0c7f04307e25ee970
riazonbin <riazonbin@mail.ru>
2023-03-10 11:03:48 +0300

    added list tiles

--- lib/pages/drawer.dart
+++ lib/pages/drawer.dart
@@ -5,12 +5,36 @@ class DrawerHomeMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const catUrl =
+        'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkjL46s7_3ICJVs2fH4uuqCaaKTM5SRkZCeTgDn6uOyic';
     return Drawer(
       child: ListView(
-        children:  const [
+        children: [
           UserAccountsDrawerHeader(
-              accountName: Text("Аккаунт"), accountEmail: Text("Почта"),
-              currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage('https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkjL46s7_3ICJVs2fH4uuqCaaKTM5SRkZCeTgDn6uOyic')),)
+              accountName: const Text("Аккаунт"),
+              accountEmail: const Text("Почта"),
+              otherAccountsPictures: [IconButton(onPressed:() {
+              }, icon: Icon(Icons.exit_to_app))],
+              currentAccountPicture:
+                  const CircleAvatar(backgroundImage: NetworkImage(catUrl))),
+          ListTile(
+            title: const Text("Настройки"),
+            trailing: const Icon(Icons.arrow_right),
+            leading: const Icon(Icons.settings),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text("Магазин"),
+            trailing: const Icon(Icons.arrow_right),
+            leading: const Icon(Icons.shop),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text("Помощь"),
+            trailing: const Icon(Icons.arrow_right),
+            leading: const Icon(Icons.help),
+            onTap: () {},
+          ),
         ],
       ),
     );
